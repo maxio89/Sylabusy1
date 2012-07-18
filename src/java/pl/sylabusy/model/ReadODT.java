051aef7db18f356d75230a5156f1cbb1e66b53b8
Piotr Kozłowski <maxio89@gmail.com>
2012-07-18 09:31:49 +0200

    0.1

--- src/java/pl/sylabusy/model/ReadODT.java
+++ src/java/pl/sylabusy/model/ReadODT.java
@@ -4,7 +4,12 @@
  */
 package pl.sylabusy.model;
 
+import java.util.List;
+import org.odftoolkit.simple.table.Table;
+import org.odftoolkit.odfdom.dom.element.table.TableTableElement;
 import org.odftoolkit.simple.TextDocument;
+import org.odftoolkit.simple.table.Cell;
+import org.odftoolkit.simple.text.Paragraph;
 
 /**
  *
@@ -14,7 +19,21 @@ public class ReadODT {
     
     public void loadDocument() throws Exception {
         
-        TextDocument odtDoc = TextDocument.loadDocument("C:/in.odt");
+        TextDocument odtDoc = TextDocument.loadDocument("C:/Sylabusy/PWSZ-ZI (Semestr 1, Algebra liniowa).odt");
+        
+        Paragraph aaa = odtDoc.getParagraphByIndex(2, true);
+        
+        List<Table> tables = odtDoc.getTableList();
+        
+        
+        TableTableElement el;
+        //el = (TableTableElement)tables.get(0);
+        Table tab = tables.get(0);
+        Cell cl = tab.getCellByPosition(0, 0);
+        //TableTableElement el = (TableTableElement)tables.;
+        
+        //System.out.println(aaa.getTextContent() + " - " + tables.size() + tab.getRowCount() + tab.getColumnCount() + cl.getStringValue());
+        System.out.println(tables.size() + " - " + tab.getRowCount() + " - " + tab.getColumnCount() + " - " + cl.getStringValue());
         
     }
     
